d9e3c18e16ca04a522ff559f8ffc259239e33049
thaitranquang2004 <smon24690@gmail.com>
2025-12-18 03:14:19 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -116,7 +116,7 @@
                                 Deploy now
                             </a>
                             <a href="{{ url('/san-pham') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                                Xem sản phẩm
+                                Xem sản phẩm nè
                             </a>
                         </li>
                     </ul>
